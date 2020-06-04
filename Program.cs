f4cb5fc1fa778f3f80cdecd9b00809bb3aae45d0
HenryKasai <henrykasai1775@gmail.com>
2020-06-04 11:33:30 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Insira um número inteiro.");
+
+            int número = Convert.ToInt32(Console.ReadLine()); 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            if (número < 0) 
+            {
+                Console.WriteLine($"O número {número} é negativo");
+            }
+            else if (número == 0)
+            {
+                Console.WriteLine($"O número {número} é zero");
+            }
+            else if (número > 0)
+            {
+                Console.WriteLine($"O número {número} é positivo.");
+            }
+            else 
+            {
+                Console.WriteLine("número invalido.");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+
+        }
+    }
+}
